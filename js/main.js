b7b59cd9cdd645784c1ccf708795f648cc35a584
Marcelo1305 <97449319+Marcelo1305@users.noreply.github.com>
2024-06-10 17:55:49 -0300

    correção e animação da cabeça de leitura

--- js/main.js
+++ js/main.js
@@ -1,62 +1,50 @@
 function transition(word) {
     if (validateWord(word)) {
-
-        /* Adicina cada um dos caracteres da palavra em um array 
-         * juntamente com um valor em branco para a máquina entender
-         * o fim da palavra de entrada durante a leitura */
         let fragmentedWord = [...word.split(''), ''];
-
-        /* Declaração de variáveis */
         let count = 0, position = 0, currentState = 0;
         let symbol, move, next_state, write;
 
-        /* Enquanto o estado não for o final ou se a palavra não for um palíndromo
-         * Serão percorridos os estados da máquina */
-        while (currentState != (turing.states.length - 1)) {
+        (async function() {
+            while (currentState != (turing.states.length - 1)) {
+                symbol = turing.states[currentState][fragmentedWord[position]];
+                showSteps(symbol, count, currentState, fragmentedWord[position]);
 
-            /* Essa variável recebe o objto do JSON conforme o símbolo lido */
-            symbol = turing.states[currentState][fragmentedWord[position]];
+                if (symbol == undefined) {
+                    defineFail(word);
+                    break;
+                }
 
-            /* Exibe o passo a passo do que está acontecendo na máquina */
-            showSteps(symbol, count, currentState, fragmentedWord[position]);
-            
-            /* Se não for um palíndromo entra aqui */
-            if (symbol == undefined) {
-                defineFail(word);
-                break;
+                move = symbol.move;
+                next_state = symbol.next_state;
+                write = symbol.write;
+
+                currentState = next_state;
+                fragmentedWord[position] = write;
+
+                position = move == 'right' ? position + 1 : position - 1;
+                moveHead(position);
+
+                count++;
+
+                if (currentState == (turing.states.length - 1)) defineSuccess(word, count);
+
+                await new Promise(resolve => setTimeout(resolve, 1000));
             }
-
-            /* Atualiza as variáveis com os valores do JSON */
-            move = symbol.move;
-            next_state = symbol.next_state;
-            write = symbol.write;
-
-            /* Altera o estado atual*/
-            currentState = next_state;
-
-            /* Escreve no Array o valor de escrita da máquina */
-            fragmentedWord[position] = write;
-
-            /* Movimenta para esquerda ou direita no Array*/
-            position = move == 'right' ? position + 1 : position - 1;
-
-            /* Incrementa o contador de operações na fita */
-            count++;
-            
-            if (currentState == (turing.states.length - 1)) defineSuccess(word, count);
-        }
-        /* Se o alfabeto for inválido entra aqui */
-    } else defineFail(word);
+        })();
+    } else {
+        defineFail(word);
+    }
 }
 
-/* 
- * Validação da entrada de dados conforme o alfabeto de entrada da máquina
- */
 function validateWord(word) {
-    let valueReturn = false;
-    for (const i in word) {
-        if (word.charAt(i) != 0 && word.charAt(i) != 1) valueReturn = false;
-        else valueReturn = true;
-    }
-    return valueReturn;
+    return /^[01]+$/.test(word);
+}
+
+function moveHead(position) {
+    var cellWidth = 48; // Largura de cada célula na fita
+    var offset = 2; // Ajuste este valor conforme necessário
+    var newLeftPosition = position * cellWidth + offset;
+
+    // Mova a cabeça de leitura para a nova posição
+    $('#head').css('left', newLeftPosition + 'px');
 }
